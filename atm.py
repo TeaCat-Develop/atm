249fc57c7a376169aadc310080fc1fa509a6715e
TeaCat-Develop <TeaCat0416@gmail.com>
2025-07-18 13:24:56 +0900

    ✨ 영수증 보기 기능 추가
    입출금내역을 저장하고 영수증 내역으로 출력 가능한 기능 추가

--- atm.py
+++ atm.py
@@ -3,7 +3,8 @@
 # balance : 초기 잔액을 설정하는 변수를 초기화 = 할당
 # 금액은 자유
 
-balance = 10000 # 잔액 초기화
+balance = 10000 # 잔액 초기화 = 사용하기 위해 미리 만드는 것. 특정값을 넣어서 만들거나 빈 값을 넣어 만듦.
+receipts = []
 
 # 사용자로부터 atm 기기의 사용 목적에 맞는 기능을 선택할 수 있도록
 # 기능 입력을 받는 기능을 구현
@@ -26,22 +27,36 @@ while True : # 무한반복
         deposit_amount = int(input('입금할 금액을 입력해주세요.')) # str:5000 -> int -> int:5000
         balance += deposit_amount
         print(f'입금하신 금액은{deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.')
+        receipts.append( ('입금하신 금액', deposit_amount, balance) )
+        #.append()에 소괄호 또 넣은 이유: 튜플(보안을 위해)
+        print(receipts)
+
     
     if num == '2': # 출금 기능 구현 -> feat/withdraw 브랜치에서 작업
         withdraw_amount = int(input('출금할 금액을 입력해주세요.')) # str:5000 -> int -> int:5000
         withdraw_amount = min(balance,withdraw_amount) # 내장함수 min
         balance -= withdraw_amount
         print(f'{withdraw_amount}원 출금되었습니다. 현재 잔액은 {balance}원 입니다.')
-        # min=가벼움 if=4시인성
-        # if balance>0:
-        #     print(f'출금하신 금액은{withdraw_amount}원이고, 현재 잔액은 {balance}원 입니다.')
-        # else:
-        #     balance+=withdraw_amount
-        #     print('잔액이 부족합니다.')
+        receipts.append( ('출금하신 금액', withdraw_amount, balance) )
+                  # min=가벼움 if=시인성
+            # if balance>0:
+            #     print(f'출금하신 금액은{withdraw_amount}원이고, 현재 잔액은 {balance}원 입니다.')
+            # else:
+            #     balance+=withdraw_amount
+            #     print('잔액이 부족합니다.')
     
     if num == '3':
-        pass
-    # elif가 아니라 if를 사용하는 이유 -> 시인성
-    # 전부 if 사용이 가능한 이유: 코드는 절차적으로 실행되므로...
+        if receipts: # 값이 존재하면 true로 인식
+
+            # [('입금'),3000,13000),출금()...]
+            for i in receipts:
+                # i = ('입금',3000,13000)
+                # i[0]=>'입금'
+                print(f'{i[0]}:{i[1]}원 | 잔액 {i[2]}')
+            #   print(receipts)
+        # elif가 아니라 if를 사용하는 이유 -> 시인성
+        # 전부 if 사용이 가능한 이유: 코드는 절차적으로 실행되므로...
+        else:
+            print('영수증 내역이 없습니다')
 
 print(f'서비스를 종료합니다. 현재 잔액은 {balance} ₩ 입니다.')
